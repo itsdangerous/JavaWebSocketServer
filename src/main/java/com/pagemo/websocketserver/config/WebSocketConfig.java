a888cd740de21fa77cba785aa50dd8d26007f621
itsdangerous <danger121@naver.com>
2023-07-25 02:48:03 +0900

    feat: buffer 사이즈 설정 추가

--- src/main/java/com/pagemo/websocketserver/config/WebSocketConfig.java
+++ src/main/java/com/pagemo/websocketserver/config/WebSocketConfig.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,7 +21,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws").setAllowedOrigins("*", "http://localhost:3000");
+    }
+
+    // buffer 사이즈 제한
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(512 * 1024);  // 512KB
+        registration.setSendBufferSizeLimit(1024 * 1024);  // 1MB
     }
 
 
