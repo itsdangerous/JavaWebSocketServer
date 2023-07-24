19b644d8ceb52773f6dd220104034c0b7ea25da7
itsdangerous <danger121@naver.com>
2023-07-24 22:56:34 +0900

    refactor: ImageController 주석 수정

--- src/main/java/com/pagemo/websocketserver/controller/ImageController.java
+++ src/main/java/com/pagemo/websocketserver/controller/ImageController.java
@@ -27,16 +27,21 @@ public class ImageController {
     // 자바 서버는 /topic/imageData를 구독하고 있는 파이썬 클라이언트에게 이미지 데이터를 전송.
     @MessageMapping("/imageData")
     public void processImageDataFromReactClient(@Payload byte[] imageBytes) {
-        long end = System.currentTimeMillis();
-        this.template.convertAndSend("/topic/imageData", imageBytes); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
-        System.out.println("이미지데이터를 파이썬 클라이언트에게 성공적으로 보냈음.");
+        if (imageBytes.length > 0) {
+                System.out.println("이미지 데이터를 리액트 클라이언트로부터 받음. ");
+            // /topic/imageData를 구독하는 파이썬 클라이언트에게 이미지 데이터 전송
+            this.template.convertAndSend("/topic/imageData", imageBytes);
+            System.out.println("이미지데이터를 파이썬 클라이언트에게 성공적으로 보냈음.");
+        };
     }
 
     // /app/analyze 경로로 파이썬 클라이언트가 결과를 던져주면,
     // /topic/analyze리액트 클라이언트에게 결과를 전송
     @MessageMapping("/analyzingData")
     public void receiveAnalyzingDataFromPythonClient(@Payload Map<String, Object> analyzingData) {
-        this.template.convertAndSend("/topic/analyzingData", analyzingData); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
+
+        // /topic/analyzingData를 구독하는 리액트 클라이언트에게 이미지 데이터 전송
+        this.template.convertAndSend("/topic/analyzingData", analyzingData);
 
     }
 
