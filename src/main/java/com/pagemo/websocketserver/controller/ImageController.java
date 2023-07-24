6453d43e94b9a15ac36b9f7b51299b57ba228723
itsdangerous <danger121@naver.com>
2023-07-24 16:20:31 +0900

    feat: 구독 및 send uri 수정

--- src/main/java/com/pagemo/websocketserver/controller/ImageController.java
+++ src/main/java/com/pagemo/websocketserver/controller/ImageController.java
@@ -7,12 +7,11 @@ package com.pagemo.websocketserver.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import java.util.Arrays;
 import java.util.Map;
 
 
@@ -25,19 +24,22 @@ public class ImageController {
         this.template = template;
     }
 
-    @PostMapping("/image")
-    public String image(@RequestBody byte[] imageBytes) {
-        start = System.currentTimeMillis();
-        this.template.convertAndSend("/topic/image", imageBytes); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
-        return "이미지 잘 받음";
-
+    // 리액트 클라이언트가 /app/imageData 경로로 이미지 데이터를 전송하면
+    // 자바 서버는 /topic/imageData를 구독하고 있는 파이썬 클라이언트에게 이미지 데이터를 전송.
+    @MessageMapping("/imageData")
+    public void processImageDataFromReactClient(@Payload byte[] imageBytes) {
+        long end = System.currentTimeMillis();
+        System.out.println("걸린 시간 : " + (end - start) + "ms");
+        this.template.convertAndSend("/topic/imageData", imageBytes); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
+        System.out.println("이미지데이터를 파이썬 클라이언트에게 성공적으로 보냈음.");
     }
 
-    @MessageMapping("/analyze")
-    public void processMessageFromClient(@Payload Map<String, Object> messageData) {
-        long end = System.currentTimeMillis();
-        System.out.println("서버가 받은 이미지 분석 결과: " + messageData);
-        System.out.println("걸린 시간 : " + (end - start) + "ms");
+    // /app/analyze 경로로 파이썬 클라이언트가 결과를 던져주면,
+    // /topic/analyze리액트 클라이언트에게 결과를 전송
+    @MessageMapping("/analyzingData")
+    public void receiveAnalyzingDataFromPythonClient(@Payload Map<String, Object> analyzingData) {
+        this.template.convertAndSend("/topic/analyzingData", analyzingData); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
+
     }
 
 }
--- src/main/java/com/pagemo/websocketserver/controller/MessageController.java
+++ src/main/java/com/pagemo/websocketserver/controller/MessageController.java
@@ -42,10 +42,16 @@ public class MessageController {
 //    클라이언트가 /app/send로 메시지를 보낼 때마다
 //    그 메시지를 그대로 /topic/messages 주제를 구독하는 모든 클라이언트에게 보냄
     @MessageMapping("/receive")
-    public String processMessageFromClient(@Payload Map<String, String> messageData) throws Exception {
+    public void processMessageFromClient(@Payload Map<String, String> messageData) throws Exception {
         // 메시지 데이터를 출력
         System.out.println("모델 클라이언트에서 받은 메세지 : " + messageData);
-
-        return messageData.toString();
+        this.template.convertAndSend("/topic/messages", "잘 받았다. ㅋㅋ"); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
     }
+
+    @MessageMapping("/message")
+    public void processMessageFromAllClient(@Payload String messageData) {
+        this.template.convertAndSend("/topic/message", messageData); // http post 요청의 본문을 웹소켓 클라이언트에게 보냄
+        System.out.println("클라이언트로부터 데이터 받음 : " + messageData);
+    }
+
 }
