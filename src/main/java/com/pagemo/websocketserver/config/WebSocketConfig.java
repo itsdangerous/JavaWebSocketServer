30876d4eb6b373181b7f525e185f1d194e093460
itsdangerous <danger121@naver.com>
2023-07-25 12:36:26 +0900

    feat: 웹소켓 연결 디버깅 추가

--- src/main/java/com/pagemo/websocketserver/config/WebSocketConfig.java
+++ src/main/java/com/pagemo/websocketserver/config/WebSocketConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,7 +24,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*", "http://localhost:3000");
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .addInterceptors(new HandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(org.springframework.http.server.ServerHttpRequest request, org.springframework.http.server.ServerHttpResponse response, org.springframework.web.socket.WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        System.out.println("클라이언트가 연결을 시도합니다: " + request.getRemoteAddress());
+                        return true;
+                    }
+
+                    @Override
+                    public void afterHandshake(org.springframework.http.server.ServerHttpRequest request, org.springframework.http.server.ServerHttpResponse response, org.springframework.web.socket.WebSocketHandler wsHandler, Exception exception) {
+                        System.out.println("Handshake 완료: " + request.getRemoteAddress());
+                    }
+                })
+                .withSockJS();
     }
 
     // buffer 사이즈 제한
